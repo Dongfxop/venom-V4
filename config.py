c89588a0a27af17375d3e27f0dd11bbb09cfbc5d
Dongfxop <107860354+Dongfxop@users.noreply.github.com>
2024-07-22 02:15:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6922683972:AAF9QRdess690T8HBBC2Nf1gH2Y35h_kLHc")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("dongfxop")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6079904466")
 
 
 
